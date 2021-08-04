e05cf4d382de36d109b4fff5a72463bb4f0a4eff
jadelien1618 <jl9653@nyu.edu>
2021-08-04 11:11:58 +0800

    deleteiframe

---
+++ assets/main_assets/nav.js
@@ -0,0 +1,90 @@
+﻿
+document.write("<div id='left' class='scrolling-wrapper-left'>");
+document.write("<header>");
+document.write("<a class='header' href='index.html'>");
+document.write("JADE LIEN &nbsp 連亞珏<br>");
+document.write("</a>");
+document.write("</header>");
+document.write("<nav>");
+document.write("<ul>");
+
+
+document.write("<li><a href='about.html' class='item'>");
+document.write("ABOUT Me ;)</a></li>");
+document.write("<li><a href='experiences.html' class='item'>");
+document.write("EXPERIENCES</a></li>");
+document.write("<li><a href='contact.html' class='item'>");
+document.write("CONTACT</a></li>");
+document.write("<li><a href='https://www.instagram.com/cnnmn_cnm/' target='_blank' class='item'>");
+document.write("instagram @cnnmn_cnm</a></li>");
+document.write("<li><a href='https://flipermag.com/2020/11/22/jade-lien-illustration/' target='_blank' class='item'>");
+document.write("FLiPER 生活藝文誌【不只是攝影...</a></li><br>");
+
+document.write("<li class='statement'><b>VIDEO</b></li>");
+document.write("<li><a href='strikethrough-21.html' class='item'>");
+document.write("2021 Strikethrough '21</a></li>");
+document.write("<li><a href='strikethrough.html' class='item'>");
+document.write("2020 Strikethrough</a></li>");
+document.write("<li><a href='jade-and-i.html' class='item'>");
+document.write("2019 Jade and I</a></li>");
+document.write("<li><a href='thought-experiment-the-lazy-screenwriter.html' class='item'>");
+document.write("2018 思想實驗 之 懈怠的劇作家</a></li>");
+document.write("<li><a href='whismuring.html' class='item'>");
+document.write("2017 微語 Whismuring</a></li>");
+document.write("<br>");
+
+document.write("<li class='statement'><b>PHOTOGRAPHY</b></li>");
+document.write("<li><a href='iPhone.html' class='item'>");
+document.write("iPhone</a> | <a href='digital.html' class='item'>digital</a> | ");
+document.write("<a href='analog.html' class='item'>analog</a></li>");
+document.write("<li><a href='blue-time.html' class='item'>");
+document.write("2021 Blue Time (cyanotype)</a></li>");
+document.write("<li><a href='the-look-of-flowers-that-are-looked-at.html' class='item'>");
+document.write("the look of flowers that are looked at</a></li>");
+document.write("<li><a href='dynamics-of-growth-in-a-finite-world.html' class='item'>");
+document.write("dynamics of growth in a finite world</a></li>");
+document.write("<li><a href='fossils.html' class='item'>");
+document.write("fossils</a></li>");
+document.write("<li><a href='double-self-portrait.html' class='item'>");
+document.write("double-self portrait</a></li>");
+document.write("<li><a href='wonder-wounds.html' class='item'>");
+document.write("wonder-wounds</a></li>");
+document.write("<br>");
+
+document.write("<li class='statement'><b>VISUAL CODING</b></li>");
+document.write("<li><a href='image-keyword-rasterizer.html' class='item'>");
+document.write("image-keyword rasterizer</a></li>");
+document.write("<li><a href='slitscan-stereographic-projection.html' class='item'>");
+document.write("slitscan stereographic projection</a></li>");
+document.write("<li><a href='audiovisual-sound-stamps.html' class='item'>");
+document.write("audiovisual sound stamps</a></li>");
+document.write("<br>");
+
+document.write("<li class='statement'><b>CREATIVE WRITING</b></li>");
+document.write("<li class='statement'>coming soon... MIA</li>");
+document.write("<li><a href='子宮穹蒼之下.html' class='item'>");
+document.write("2021 子宮穹蒼之下</a></li>");
+document.write("<li><a href='裸體性論述.html' class='item'>");
+document.write("2020 裸體性論述</a></li>");
+document.write("<li><a href='the-bitumen-drips.html' class='item'>");
+document.write("2019 The Bitumen Drips</a></li>");
+document.write("<li><a href='散文詩.html' class='item'>");
+document.write("散文詩 archive</a></li>");
+document.write("<br>");
+
+document.write("<li class='statement'><b>OTHERS</b></li>");
+document.write("<li><a href='thesis-antithesis-synthesis-with-stick-figures.html' class='item'>");
+document.write("thesis, antithesis, synthesis with stick...</a></li>");
+document.write("<li><a href='animation-flipbook.html' class='item'>");
+document.write("animation flipbook</a></li>");
+document.write("<li><a href='vocalization-project-walking-and-falling.html' class='item'>");
+document.write("vocalization project: walking and falling</a></li>");
+
+document.write("<br><br><br>");
+
+document.write("<li><div class='copyright'>© 2021 cnnmn_cnm.</div></li>");
+    
+document.write("</ul>");
+document.write("</nav>");
+
+document.write("</div>");
